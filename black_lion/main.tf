7c031ee0741c7ee9ceb7be4ad2a3ecd8fb4952d0
Tim Fairweather <tim.fairweather@arctiq.ca>
2018-11-28 11:20:32 -0500

    added routine for vault IP

--- black_lion/main.tf
+++ black_lion/main.tf
@@ -17,12 +17,15 @@ variable "vault_path" {}
 variable "vault_cluster_name" {}
 variable "disable_clustering" {}
 variable "vault_server_log_level" {}
+
 variable "consul_server_ips" {
   type = "list"
 }
+
 variable "consul_client_ips" {
   type = "list"
 }
+
 variable "vault_oss_instance_count" {}
 variable "vault_custom_instance_count" {}
 variable "vault_custom_config_template" {}
@@ -82,6 +85,7 @@ data "template_file" "vault_tls_key" {
 
 data "template_file" "telemetry_config" {
   template = "${file("${path.module}/templates/vault_telemetry.tpl")}"
+
   vars {
     statsd_ip = "${var.statsd_ip}"
   }
@@ -95,66 +99,68 @@ resource "docker_container" "vault_oss_server" {
   count = "${var.vault_oss_instance_count}"
   name  = "vaultron-${format("vault%d", count.index)}"
   image = "${docker_image.vault.latest}"
-  entrypoint = ["vault", "server", "-log-level=${var.vault_server_log_level}", "-config=/vault/config"]
-  env      = ["VAULT_CLUSTER_INTERFACE=eth0",
-              "VAULT_REDIRECT_INTERFACE=eth0"]
-  hostname  = "${format("vault%d", count.index)}"
+
+  # entrypoint = ["vault", "server", "-log-level=${var.vault_server_log_level}", "-config=/vault/config"]
+
+
+  # entrypoint = ["VAULT_API_ADDR=$(awk 'END{print $1}' /etc/hosts)", "vault", "server", "-log-level=${var.vault_server_log_level}", "-config=/vault/config"]
+
+  command = [
+    "sh",
+    "-c",
+    "VAULT_API_ADDR='http://$(ip addr show eth0 | grep global | awk '{ print $2 }' | awk -F '/' '{ print $1}'):8200' VAULT_CLUSTER_ADDR='https://$(ip addr show eth0 | grep global | awk '{ print $2 }' | awk -F '/' '{ print $1}'):8201'; vault server -log-level=${var.vault_server_log_level} -config=/vault/config",
+  ]
+  env = ["VAULT_CLUSTER_INTERFACE=eth0",
+    "VAULT_REDIRECT_INTERFACE=eth0",
+  ]
+  hostname   = "${format("vault%d", count.index)}"
   domainname = "consul"
   dns        = ["${var.consul_server_ips}"]
   dns_search = ["consul"]
-  labels = { robot = "vaultron" }
+  labels = {
+    robot = "vaultron"
+  }
   must_run = true
-
   capabilities {
     add = ["IPC_LOCK"]
   }
-
   volumes {
     host_path      = "${path.module}/../../../vault/vault${count.index}/audit_log"
     container_path = "/vault/logs"
   }
-
   volumes {
     host_path      = "${path.module}/../../../vault/vault${count.index}/config"
     container_path = "/vault/config"
   }
-
   volumes {
     host_path      = "${path.module}/../../../vault/plugins"
     container_path = "/vault/plugins"
   }
-
   upload {
     content = "${element(data.template_file.vault_config.*.rendered, count.index)}"
     file    = "/vault/config/main.hcl"
   }
-
   upload {
     content = "${data.template_file.telemetry_config.rendered}"
     file    = "${ var.vaultron_telemetry_count ? "/vault/config/telemetry.hcl" : "/tmp/telemetry.hcl" }"
   }
-
   upload {
     content = "${data.template_file.ca_bundle.rendered}"
     file    = "/etc/ssl/certs/ca-bundle.pem"
   }
-
   upload {
     content = "${element(data.template_file.vault_tls_cert.*.rendered, count.index)}"
     file    = "/etc/ssl/certs/vault-server.crt"
   }
-
   upload = {
     content = "${element(data.template_file.vault_tls_key.*.rendered, count.index)}"
     file    = "/etc/ssl/vault-server.key"
   }
-
   ports {
     internal = "8200"
     external = "${format("82%d0", count.index)}"
     protocol = "tcp"
   }
-
 }
 
 # -----------------------------------------------------------------------
@@ -205,17 +211,24 @@ data "template_file" "vault_custom_config" {
 # -----------------------------------------------------------------------
 
 resource "docker_container" "vault_custom_server" {
-  count = "${var.vault_custom_instance_count}"
-  name  = "vaultron-${format("vault%d", count.index)}"
-  image = "${docker_image.vault.latest}"
+  count      = "${var.vault_custom_instance_count}"
+  name       = "vaultron-${format("vault%d", count.index)}"
+  image      = "${docker_image.vault.latest}"
   entrypoint = ["/vault/custom/vault", "server", "-log-level=${var.vault_server_log_level}", "-config=/vault/config"]
-  env      = ["VAULT_CLUSTER_INTERFACE=eth0",
-              "VAULT_REDIRECT_INTERFACE=eth0"]
-  hostname  = "${format("vault%d", count.index)}"
+
+  env = ["VAULT_CLUSTER_INTERFACE=eth0",
+    "VAULT_REDIRECT_INTERFACE=eth0",
+  ]
+
+  hostname   = "${format("vault%d", count.index)}"
   domainname = "consul"
   dns        = ["${var.consul_server_ips}"]
   dns_search = ["consul"]
-  labels = { robot = "vaultron" }
+
+  labels = {
+    robot = "vaultron"
+  }
+
   must_run = true
 
   capabilities {
@@ -262,20 +275,19 @@ resource "docker_container" "vault_custom_server" {
     file    = "/etc/ssl/certs/ca-bundle.pem"
   }
 
-   upload {
-     content = "${element(data.template_file.vault_custom_tls_cert.*.rendered, count.index)}"
-     file    = "/etc/ssl/certs/vault-server.crt"
-   }
+  upload {
+    content = "${element(data.template_file.vault_custom_tls_cert.*.rendered, count.index)}"
+    file    = "/etc/ssl/certs/vault-server.crt"
+  }
 
-   upload {
-     content = "${element(data.template_file.vault_custom_tls_key.*.rendered, count.index)}"
-     file    = "/etc/ssl/vault-server.key"
-   }
+  upload {
+    content = "${element(data.template_file.vault_custom_tls_key.*.rendered, count.index)}"
+    file    = "/etc/ssl/vault-server.key"
+  }
 
   ports {
     internal = "8200"
     external = "${format("82%d0", count.index)}"
     protocol = "tcp"
   }
-
 }
